d4c1563e1bc353058715c032f033fc7ff9521079
tervingo <keltivas@gmail.com>
2025-10-21 23:09:36 +0200

    mnemosin.netlify.app added to hosts

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI(
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://*.netlify.app"],
+    allow_origins=["http://localhost:3000", "https://mnemosin.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
